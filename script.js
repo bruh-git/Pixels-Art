bbe929d2879d1d4992d944551663b38106929cac
Bruna <brunac.cardososilva@gmail.com>
2021-12-17 15:20:46 -0300

    iniciando requesito 7

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
-// let pixels = document.getElementsByClassName("pixel");
-// for (let i =0; i <=25; i++){
-//     pixels.innerHTML += '<div class="pixel"></div>';
-// }
+
+// adiciona eventos em varios elementos
+const colorPalette = document.querySelector('#color-palette');
+const pixelBoard = document.querySelector('#pixel-board');
+const color = document.querySelectorAll('.color');
+const pixel = document.querySelectorAll('.pixel');
+
+for (let i = 0; i < color.length; i += 1){ 
+    color[i].addEventListener('click',selecionaCor);
+}
+
+function selecionaCor(event){
+    const seleciona = document.querySelector('.selected');
+    seleciona.classList.remove('selected');
+    event.target.classList.add('selected');
+}
+// function mudaBackground() {
+//     document.getElementsByClassName('.color').style.backgroundColor = selecionaCor;
+//     }
